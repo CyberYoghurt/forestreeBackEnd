137fafcf33252f7e676a1ec0107fd5e4dacb9910
CyberYoghurt <robertocr.21@hotmail.com>
2023-07-09 18:10:20 -0600

    Stable, future refactor is needed

--- forestreeApp/webserver/views.py
+++ forestreeApp/webserver/views.py
@@ -176,11 +176,10 @@ class CheckOut(APIView):
 
 
 class ChatInfo(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
 
     # Get the chats where is a user has joined
     def get(self, request):
-        print('trying chat info')
         user = request.user
         chats = user.user_chats.all()
         chats_serialized = ChatListSerializer(chats, many=True)
