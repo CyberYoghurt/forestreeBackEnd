1f29758f1d467bb05694f218b148dd83f70fb458
CyberYoghurt <robertocr.21@hotmail.com>
2023-07-23 21:49:38 -0600

    Trying to connect to PostgreSQL

--- forestreeApp/forestreeApp/settings.py
+++ forestreeApp/forestreeApp/settings.py
@@ -127,8 +127,12 @@ ASGI_APPLICATION = 'forestreeApp.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qfzevbig',
+        'USER': 'qfzevbig',
+        'PASSWORD': 'bkUMw6l5pytWfI_ejeMRC_TvFHMBoHpi',
+        'HOST': 'mahmud.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
